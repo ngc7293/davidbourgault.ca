5201ffbe3172890e14a9e7aec98514815703bca8
David Bourgault <dav.bour@gmail.com>
2021-02-02 20:44:32 -0500

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,193 @@
+var locale = {
+    hero: {
+        subtitle: { fr: 'Développeur', en: 'Developer' }
+    },
+    cv: {
+        sections: [
+            {
+                title: { fr: 'Expérience', en: 'Experience' },
+                items: [
+                    {
+                        dates: [
+                            {
+                                start: '2020/01',
+                                end: '2020/08',
+                                title: { fr: 'Temps partiel', en: 'Part-time' }
+                            },
+                            {
+                                start: '2019/09',
+                                end: '2019/12',
+                                title: { fr: 'Stage', en: 'Internship' }
+                            }
+                        ],
+                        title: { fr: 'Développeur logiciel', en: 'Software developer' },
+                        org: {
+                            name: 'Broadsign International LLC',
+                            link: { fr: 'https://broadsign.com/fr', en:'https://broadsign.com' }
+                        },
+                        tasks: [
+                            { fr: 'Contribuer à un serveur web haute-performance', en: 'Contribute to a high-performance web server in C++'},
+                            { fr: 'Concevoir des nouvelles architecturers et composantes', en: 'Design new software components and architectures'},
+                            { fr: 'Développer des applications de bureau avec Qt', en: 'Develop Qt-based desktop applications'},
+                            { fr: 'Contribuer à une infrastructure de tests automatisés', en: 'Contribute to an automated testing framework'},
+                            { fr: 'Investiger et corriger les bogues', en: 'Investigate and resolve bugs and client issues'}
+                        ],
+                        tools: ['C++', 'Qt', 'CMake', 'Google Test', 'AWS', 'Python', 'PostgreSQL']
+                    },
+                    {
+                        dates: [
+                            {
+                                start: '2018/09',
+                                end: '2018/12',
+                                title: { fr: 'Stage', en: 'Internship' }
+                            },
+                        ],
+                        title: { fr: 'Développeur en embarqué et en test', en: 'Embedded & test developer' },
+                        org: {
+                            name: 'Silicon Laboratories Inc.',
+                            link: { fr: 'https://www.silabs.com/', en:'https://www.silabs.com/' }
+                        },
+                        tasks: [
+                            { fr: 'Contribuer au kernel Micrium', en: 'Contribute to the Micrium real-time kernel'},
+                            { fr: 'Développer une infrastructure de profilage automatisée', en: 'Develop an automated profiling infrastructure'},
+                            { fr: 'Intégrer l\'infrastructure de profilage à Jenkins CI', en: 'Integrate profiling intrastructure with Jenkins CI'},
+                            { fr: 'Déboguer le code applicatif et système embarqué', en: 'Debug embedded application and OS code'},
+                            { fr: 'Créer des scripts de réusinage de code source', en: 'Create source code refactoring scripts'}
+                        ],
+                        tools: ['C', 'Python', 'RTOS', 'Jenkins', 'SCRUM']
+                    },
+                    {
+                        dates: [
+                            {
+                                start: '2018/01',
+                                end: '2018/07',
+                                title: { fr: 'Temps partiel', en: 'Part-time' }
+                            },
+                            {
+                                start: '2017/09',
+                                end: '2017/12',
+                                title: { fr: 'Stage', en: 'Internship' }
+                            }
+                        ],
+                        title: { fr: 'Développeur web full-stack', en: 'Full stack web developer' },
+                        org: {
+                            name: 'Solutions inLibro Inc.',
+                            link: { fr: 'https://inlibro.com/', en:'https://inlibro.com/en/' }
+                        },
+                        tasks: [
+                            { fr: 'Contribuer au SIGB open-source Koha', en: 'Contribute to the Koha open-source ILS'},
+                            { fr: 'Programmer des scripts d\'automatisation en Bash et en Python', en: 'Develop automation scripts in Bash and Python'},
+                            { fr: 'Configuer et gérer des serveurs Debian', en: 'Configure and maintain Linux/Debian servers'},
+                        ],
+                        tools: ['Perl', 'Python', 'MySQL', 'Linux', 'Bash', 'XML', 'HTML/CSS']
+                    }
+                ]
+            },
+            {
+                title: { fr: 'Éducation', en: 'Education' },
+                items: [
+                    {
+                        dates: [
+                            {
+                                start: '2017/05',
+                                end: '2021/04',
+                                title: { fr: '', en: '' }
+                            }
+                        ],
+                        title: { fr: 'Baccalauréat en Génie Logiciel', en: 'Bachelor of Software Engineering' },
+                        org: {
+                            name: 'École de Technologie Supérieure',
+                            link: { fr: 'https://etsmtl.ca', en:'https://etsmtl.ca/en/home' }
+                        },
+                        tasks: [
+                            { fr: 'Mention Génie+ pour implication dans la vie étudiante', en: 'Génie+ designation for involvement in student life'},
+                        ],
+                        tools: []
+                    },
+                    {
+                        dates: [
+                            {
+                                start: '2016/08',
+                                end: '2017/04',
+                                title: { fr: '', en: '' }
+                            }
+                        ],
+                        title: { fr: 'Cheminement technologique universitaire', en: 'Technological Academic Path' },
+                        org: {
+                            name: 'École de Technologie Supérieure',
+                            link: { fr: 'https://etsmtl.ca', en:'https://etsmtl.ca/en/home' }
+                        },
+                        tasks: [],
+                        tools: []
+                    },
+                    {
+                        dates: [
+                            {
+                                start: '2013',
+                                end: '2016',
+                                title: { fr: '', en: '' }
+                            }
+                        ],
+                        title: { fr: 'DEC en Sciences Informatiques et Mathématiques', en: 'College Diploma in Computer Science & Mathematics' },
+                        org: {
+                            name: 'CÉGEP Limoilou',
+                            link: { fr: 'https://www.cegeplimoilou.ca/', en:'https://www.cegeplimoilou.ca/' }
+                        },
+                        tasks: [],
+                        tools: []
+                    }
+                ]
+            },
+            {
+                title: { fr: 'Projets', en: 'Projects' },
+                items: [
+                    {
+                        dates: [
+                            {
+                                start: '2017/05',
+                                end: '2020/12',
+                                title: { fr: 'Développeur', en: 'Developer' }
+                            },
+                            {
+                                start: '2019/07',
+                                end: '2020/01',
+                                title: { fr: 'Project Manager', en: 'Project Manager' }
+                            }
+                        ],
+                        title: { fr: 'RockÉTS — Fusées haute puissance', en: 'RockÉTS — High Powered Rockets' },
+                        org: {
+                            name: 'École de Technologie Supérieure',
+                            link: { fr: 'https://clubrockets.ca/', en:'https://clubrockets.ca/en' }
+                        },
+                        tasks: [
+                            { fr: 'Lancement de 5 fusées sondes à 3km et 9km', en: 'Launched 5 sounding rockets to 10k and 30k feet'},
+                            { fr: 'Gérer le projet de rearchitecture de l\'avionique 2019', en: 'Manage the 2019 avionics redesign project'},
+                            { fr: 'Créer un logiciel de contrôle et d\'affichage de la télémétrie', en: 'Create a ground control station program (monitoring and control)'},
+                            { fr: 'Programmer les systèmes embarqués', en: 'Develop real-time software for onboard systems'},
+                            { fr: 'Créer les procédures pour le lancement et l\'assemblage', en: 'Create procedures for launch and assembly operations'},
+                            { fr: 'Assembler l\'avionique pour le lancement', en: 'Assemble avionics prior to launch'}
+                        ],
+                        tools: ['C', 'C++', 'FreeRTOS', 'STM32', 'Qt', 'SPI', 'I₂C', 'Altium']
+                    },
+                ]
+            }
+        ]
+    }
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        lang: 'fr',
+        loc: locale
+    },
+    methods: {
+        setlang: function(lang) {
+            if (['fr', 'en'].includes(lang)) {
+                this.lang = lang;
+            } else {
+                this.lang = 'fr';
+            }
+        },
+    }
+})
